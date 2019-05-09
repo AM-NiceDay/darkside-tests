eb179f5c64984090f5c9b524de11fa97c25bd455
AM_NiceDay <a.krasikau@gmail.com>
2019-05-09 17:16:04 +0300

    Setup tests for todo

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,7 +1,22 @@
-describe("ping", () => {
-  it("pong", () => {
-    cy.visit("localhost")
-      .get(".App")
-      .should("have.text", "Hello world");
+describe("Test", () => {
+  it("is live", () => {
+    cy.visit("/");
+  });
+
+  it("able to create new todos", () => {
+    cy.visit("/")
+      .getByTestId("todo-form-input", { timeout: 100 })
+      .type("Test")
+      .getByTestId("todo-form-submit", { timeout: 100 })
+      .click()
+      .getByTestId("todo-form-input", { timeout: 100 })
+      .type("Test2")
+      .getByTestId("todo-form-submit", { timeout: 100 })
+      .click()
+      .get("[data-test=todo-text]")
+      .should(todos => {
+        expect(todos[0]).to.have.text("Test");
+        expect(todos[1]).to.have.text("Test2");
+      });
   });
 });
